ad3407cd6afc4d2cfd86971a883cf67eccb6405d
lac <laclys@126.com>
2017-05-09 23:43:51 +0800

    added new demo

---
+++ demo002/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'advertisement',
+    created() {
+      console.log(this.changePeople);
+    },
+    computed: {
+      changePeople() {
+        return this.$store.getters.changePeople
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+
+</style>
